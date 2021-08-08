cd029ad344d8e8238f4ec78822ca740f7a771bf1
Ivan <aipyth.func@gmail.com>
2021-08-08 17:39:14 +0300

    c task 5 done

---
+++ courses/cunix/ex05/src/my_puts.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+int my_puts(const char *s) {
+    // get s length
+    int n = 0;
+    while (s[n++] != '\0') {}
+    int ret = write(1, s, n-1);
+    write(1, "\n", 1);
+    return ret;
+}
